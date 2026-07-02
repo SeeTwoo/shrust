aedced3fc9631e62ecb55908aae5d90ca6fe7b48
SeeTwoo <waltibee@gmail.com>
2026-07-02 10:03:37 +0200

    very basic display of the line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use rustyline::DefaultEditor;
+
+fn  process_line(line: String) {
+    println!("{line}");
+}
+
+fn  main() {
+    let mut rl = DefaultEditor::new().unwrap();
+
+    loop {
+        let line = rl.readline(" > ").unwrap();
+        if line == "exit" {
+            break
+        }
+        process_line(line);
+    }
 }
